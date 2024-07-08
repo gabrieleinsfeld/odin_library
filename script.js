8feccbb86fcc67ee808e8dc79a868b3c7494c06c
gabrieleinsfeld <gabrieleinsfeld@gmail.com>
2024-07-08 16:58:08 -0300

    Added function to display initial card

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-console.log('test')
+
+const book1 = new Book("Harry", "Demi", "55", true)
+const book2 = new Book("Harry", "Demi", "56", true)
+const book3 = new Book("Harry", "Demi", "57", true)
+const book4 = new Book("Harry", "Demi", "58", true)
+const book5 = new Book("Harry", "Demi", "58", true)
+const book6 = new Book("Harry", "Demi", "57", true)
+const book7 = new Book("Harry", "Demi", "58", true)
+const book8 = new Book("Harry", "Demi", "58", true)
+const book9 = new Book("Harry", "Demi", "58", true)
+const book10 = new Book("Harry", "Demi", "58", true)
+
+const bookContainer = document.getElementById('books')
+let myLibrary = [book1, book2, book3, book4, book5, book6, book7, book8, book9, book10]
 
 function Book(title, author, pages, read=false){
     this.title = title 
@@ -10,7 +23,18 @@ function Book(title, author, pages, read=false){
     }
 }
 
-const book1 = new Book("Harry", "Demi", "57", true)
+
+function displayInitial(array){
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        const book = document.createElement('div')
+        book.setAttribute('class', 'book')
+        bookContainer.appendChild(book)
+    }
+
+}
 
 
+
+displayInitial(myLibrary)
 console.log(book1.showInfo())
