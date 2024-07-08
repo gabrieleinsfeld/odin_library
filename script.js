44f08b7e66c443e75aa64fa68f7265d7c315f39d
gabrieleinsfeld <gabrieleinsfeld@gmail.com>
2024-07-08 16:39:53 -0300

    Added Book Object

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('test')
+
+function Book(title, author, pages, read=false){
+    this.title = title 
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.showInfo = function (){
+        return [this.title, this.author, this.pages, this.read]
+    }
+}
+
+const book1 = new Book("Harry", "Demi", "57", true)
+
+
+console.log(book1.showInfo())
